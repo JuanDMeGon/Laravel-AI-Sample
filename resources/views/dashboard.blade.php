f49048796d4c874ccb1b88d47956744121781713
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2023-05-22 18:08:51 -0500

    Adding a dashboard view

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <!-- Include the Tailwind CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
+    <!-- Include the Alpine.js CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js"></script>
+    @livewireStyles
+</head>
+
+<body class="bg-gray-100">
+    <div class="container mx-auto px-4 py-8">
+        <h1 class="text-3xl font-bold mb-4">Dashboard</h1>
+
+    </div>
+    @livewireScripts
+</body>
+
+</html>
